f2a0fec96ed69dbd57100cd749406002e06c0f7f
“SufurElite” <13058966+SufurElite@users.noreply.github.com>
2023-10-05 14:58:48 -0400

    Added custom config location and added it to gitignore

--- netbox_agent/config.py
+++ netbox_agent/config.py
@@ -1,5 +1,6 @@
 import logging
 import sys
+import os
 
 import jsonargparse
 import pynetbox
@@ -13,6 +14,7 @@ def get_config():
             '/etc/netbox_agent.yaml',
             '~/.config/netbox_agent.yaml',
             '~/.netbox_agent.yaml',
+            os.getcwd()+'/netbox_agent/.config/netbox_agent.yaml'
         ],
         prog='netbox_agent',
         description="Netbox agent to run on your infrastructure's servers",
